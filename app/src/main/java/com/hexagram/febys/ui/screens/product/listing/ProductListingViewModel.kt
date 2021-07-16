411389f25abbe7ce0d72d05463570876ded57211
Muhammad Salman <muhammad.salman@synavos.com>
2021-07-16 15:25:40 +0500

    fix recall of category products

--- app/src/main/java/com/hexagram/febys/ui/screens/product/listing/ProductListingViewModel.kt
+++ app/src/main/java/com/hexagram/febys/ui/screens/product/listing/ProductListingViewModel.kt
@@ -1,9 +1,12 @@
 package com.hexagram.febys.ui.screens.product.listing
 
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import com.hexagram.febys.network.response.Product
 import com.hexagram.febys.repos.IProductRepo
 import com.hexagram.febys.ui.screens.product.ProductViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,7 +16,14 @@ class ProductListingViewModel @Inject constructor(
     val todayDealsListing = productRepo.fetchTodayDealsListing(viewModelScope)
     val trendingProductsListing = productRepo.fetchTrendingProductsListing(viewModelScope)
     val under100DollarsItemsListing = productRepo.fetchUnder100DollarsItemsListing(viewModelScope)
+    private var categoryProductsListing: Flow<PagingData<Product>>? = null
 
-    fun categoryProductsListing(categoryId: Int) =
-        productRepo.fetchCategoryProductsListing(categoryId, viewModelScope)
+    fun categoryProductsListing(categoryId: Int): Flow<PagingData<Product>> {
+        if (categoryProductsListing == null) {
+            categoryProductsListing =
+                productRepo.fetchCategoryProductsListing(categoryId, viewModelScope)
+        }
+
+        return categoryProductsListing!!
+    }
 }
