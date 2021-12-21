6e37f2ef2a95b0b2c588b69669641326d6e77e84
wahabahmad <wahab.ahmad@synavos.com>
2021-12-21 12:35:02 +0500

    bugs fixes

--- app/src/main/java/com/hexagram/febys/ui/screens/account/AccountFragment.kt
+++ app/src/main/java/com/hexagram/febys/ui/screens/account/AccountFragment.kt
@@ -52,7 +52,7 @@ class AccountFragment : BaseFragment() {
         }
 
         binding.orders.myOrders.setOnClickListener {
-            gotoOrderListing(null,getString(R.string.label_my_orders))
+            gotoOrderListing(arrayOf(),getString(R.string.label_my_orders))
         }
 
         binding.orders.orderReceived.setOnClickListener {
