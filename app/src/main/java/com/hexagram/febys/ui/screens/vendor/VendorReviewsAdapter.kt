d38cd0a442fcf053f0714b7c4d3d2be3105fbec9
asadullah.ahmad <asadullah.ahmad@synavos.com>
2022-02-03 12:15:56 +0500

    some fixing

--- app/src/main/java/com/hexagram/febys/ui/screens/vendor/VendorReviewsAdapter.kt
+++ app/src/main/java/com/hexagram/febys/ui/screens/vendor/VendorReviewsAdapter.kt
@@ -7,14 +7,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.hexagram.febys.databinding.ItemVendorReviewBinding
 import com.hexagram.febys.models.api.rating.VendorReview
 import com.hexagram.febys.utils.Utils
+import com.hexagram.febys.utils.load
 
 class VendorReviewsAdapter : RecyclerView.Adapter<VendorReviewsAdapter.ReviewVH>() {
     private var ratingAndReviews = listOf<VendorReview>()
 
     inner class ReviewVH(
-        private val binding: ItemVendorReviewBinding
+        private val binding: ItemVendorReviewBinding,
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: VendorReview) = binding.apply {
+            userImage.load(item.consumer?.profileImage)
             userName.text = item.consumer?.fullName
             ratingPrice.text = item.pricingScore.toInt().toString()
             ratingValue.text = item.valueScore.toInt().toString()
