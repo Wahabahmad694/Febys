0eb1c162f63f43882686372c9dc1ca991078c7fa
Muhammad Salman <muhammad.salman@synavos.com>
2022-03-11 16:37:23 +0500

    add timeline in order detail

--- app/src/main/java/com/hexagram/febys/utils/OrderStatus.kt
+++ app/src/main/java/com/hexagram/febys/utils/OrderStatus.kt
@@ -4,14 +4,14 @@ import android.graphics.Color
 
 object OrderStatus {
     const val PENDING = "PENDING"
+    const val CANCELED = "CANCELED"
+    const val REJECTED = "REJECTED"
     const val ACCEPTED = "ACCEPTED"
     const val CANCELLED_BY_VENDOR = "CANCELLED_BY_VENDOR"
     const val SHIPPED = "SHIPPED"
-    const val CANCELED = "CANCELED"
-    const val REJECTED = "REJECTED"
+    const val DELIVERED = "DELIVERED"
     const val RETURNED = "RETURNED"
     const val PENDING_RETURN = "PENDING_RETURN"
-    const val DELIVERED = "DELIVERED"
     const val REVIEWED = "REVIEWED"
 
     fun getStatusForDisplay(status: String?): String {
@@ -81,5 +81,36 @@ object OrderStatus {
         }
 
         return Color.parseColor(color)
+    }
+
+    fun isAccepted(status: String?): Boolean {
+        return when (status) {
+            PENDING,
+            REJECTED -> false
+            else -> true
+        }
+    }
+
+    fun isShipped(status: String?): Boolean {
+        return when (status) {
+            PENDING,
+            CANCELED,
+            REJECTED,
+            CANCELLED_BY_VENDOR,
+            ACCEPTED -> false
+            else -> true
+        }
+    }
+
+    fun isReceived(status: String?): Boolean {
+        return when (status) {
+            PENDING,
+            CANCELED,
+            REJECTED,
+            CANCELLED_BY_VENDOR,
+            ACCEPTED,
+            SHIPPED-> false
+            else -> true
+        }
     }
 }
---
+++ app/src/main/java/com/hexagram/febys/utils/TimelineView.kt
@@ -0,0 +1,83 @@
+package com.hexagram.febys.utils
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import com.hexagram.febys.R
+import com.hexagram.febys.databinding.ItemViewTimelineBinding
+import com.hexagram.febys.databinding.ItemViewTimelineTitleBinding
+import com.hexagram.febys.databinding.ViewTimelineBinding
+
+class TimelineView : LinearLayout {
+    val binding =
+        ViewTimelineBinding.inflate(LayoutInflater.from(context), this, true)
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int)
+            : super(context, attrs, defStyleAttr)
+
+    fun submitTimeline(timelines: List<TimelineModel>) {
+        drawTimeline(timelines)
+    }
+
+    private fun drawTimeline(timelines: List<TimelineModel>) {
+        binding.containerTimelineView.removeAllViews()
+        binding.containerTimelineTitle.removeAllViews()
+
+        val lastCompletedIndex = timelines.indexOfLast { it.isCompleted }
+
+        for ((index, timeline) in timelines.withIndex()) {
+            if (index != 0) {
+                val bgColorCode = when {
+                    timeline.isError -> "#E04C38"
+                    lastCompletedIndex == index - 1 || timeline.isCompleted -> "#6CE038"
+                    else -> "#F7F7F7"
+                }
+
+                val bgColor = Color.parseColor(bgColorCode)
+                val layoutParams = LayoutParams(0, 3, 1F)
+                val timeLineSeparator = View(context).also {
+                    it.layoutParams = layoutParams
+                    setBackgroundColor(bgColor)
+                }
+                val timeLineTitleSeparator = View(context).also { it.layoutParams = layoutParams }
+                binding.containerTimelineView.addView(timeLineSeparator)
+                binding.containerTimelineTitle.addView(timeLineTitleSeparator)
+            }
+
+            val imageRes = when {
+                timeline.isError -> R.drawable.ic_timeline_error
+                timeline.isCompleted -> R.drawable.ic_timeline_filled
+                else -> R.drawable.ic_timeline
+            }
+
+            val timelineBinding = ItemViewTimelineBinding.inflate(
+                LayoutInflater.from(context), binding.containerTimelineView, false
+            )
+            timelineBinding.ivTimeline.setImageResource(imageRes)
+            binding.containerTimelineView.addView(timelineBinding.root)
+
+            val textColorCode =
+                if (timeline.isCompleted) "#353535" else "#979797"
+            val textColor = Color.parseColor(textColorCode)
+            val timelineTitleBinding = ItemViewTimelineTitleBinding.inflate(
+                LayoutInflater.from(context), binding.containerTimelineTitle, false
+            )
+            timelineTitleBinding.tvTitleTimelineView.text = timeline.title
+            timelineTitleBinding.tvTitleTimelineView.setTextColor(textColor)
+            binding.containerTimelineTitle.addView(timelineTitleBinding.root)
+        }
+    }
+}
+
+data class TimelineModel(
+    val title: String,
+    val isCompleted: Boolean,
+    val isError: Boolean
+)
